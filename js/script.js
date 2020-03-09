7744f47d5be0406ca3e957430fcbb9865a1cfcbc
ludwnfrancoo <ludwinfrancooperador2019@gmail.com>
2020-03-09 17:20:16 -0600

    realizamos el formulario para ingresar productos

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAmPXGEFSY5mET3LsT6qQBw_-BVcLhqbTY",
+    authDomain: "maxapp-db8e1.firebaseapp.com",
+    databaseURL: "https://maxapp-db8e1.firebaseio.com",
+    projectId: "maxapp-db8e1",
+    storageBucket: "maxapp-db8e1.appspot.com",
+    messagingSenderId: "567029314884",
+    appId: "1:567029314884:web:cc3bd06f82cee91f81d311",
+    measurementId: "G-YPEQZ7J80N"
+};
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
